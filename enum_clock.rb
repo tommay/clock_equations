6aadc5fefc38c880abd6c4e02ddc17e5ae6c6e33
Tom May <tom@tommay.net>
2014-11-09 09:06:22 -0800

    Use an Enumerator to generate the times.

--- enum_clock.rb
+++ enum_clock.rb
@@ -17,12 +17,11 @@ def main
 end
 
 def time_and_equations
-  # Generate each time.
-  # Both the ranges need to use .lazy to get full laziness.
-
-  times = (1..12).lazy.flat_map do |hour|
-    (0..59).lazy.map do |minute|
-      "%d:%02d" % [hour, minute]
+  times = Enumerator.new do |y|
+    (1..12).each do |hour|
+      (0..59).each do |minute|
+        y << "%d:%02d" % [hour, minute]
+      end
     end
   end
 
