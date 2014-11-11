889d654268fb4db31d6dd410e37eccb6a752c3f7
Tom May <tom@tommay.net>
2014-11-11 00:42:59 -0800

    Separate the generic PrattParser from the evaluator.

--- pratt_evaluator.rb
+++ pratt_evaluator.rb
@@ -6,9 +6,62 @@ require "byebug"
 # http://effbot.org/zone/simple-top-down-parsing.htm
 # http://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/
 
+class PrattParser
+  def initialize(tokens)
+    @tokens = Enumerator.new do |y|
+      tokens.each do |token|
+        y << token
+      end
+      y << EndToken.new
+    end
+
+    @token = nil
+  end
+
+  def next_token
+    @tokens.next
+  end
+
+  def eval
+    @token = next_token
+    expression(0)
+  end
+
+  def expression(rbp)
+    t = @token
+    @token = next_token
+    left = t.nud(self)
+    more_expression(rbp, left)
+  end
+
+  def more_expression(rbp, left)
+    if rbp < @token.lbp
+      t = @token
+      @token = next_token
+      more_expression(rbp, t.led(self, left))
+    else
+      left
+    end
+  end
+
+  class EndToken
+    def lbp
+      0
+    end
+  end
+end
+
 class PrattEvaluator
   def self.eval(expression)
     PrattEvaluator.new(expression).eval
+  end
+
+  def initialize(expression)
+    @expression = expression
+  end
+
+  def eval
+    PrattParser.new(@expression.each_char.lazy.map{|c|@@tokens[c]}).eval
   end
 
   class Token
@@ -28,12 +81,6 @@ class PrattEvaluator
 
     def led(evaluator, left)
       left == evaluator.expression(lbp)
-    end
-  end
-
-  class EndToken
-    def lbp
-      0
     end
   end
 
@@ -93,42 +140,5 @@ class PrattEvaluator
   }
   (0..9).each do |d|
     @@tokens[d.to_s] = DigitToken.new(d.to_f)
-  end
-
-  def initialize(expression)
-    @tokens = Enumerator.new do |y|
-      expression.each_char do |c|
-        y << @@tokens[c]
-      end
-      y << EndToken.new
-    end
-
-    @token = nil
-  end
-
-  def next_token
-    @tokens.next
-  end
-
-  def eval
-    @token = next_token
-    expression
-  end
-
-  def expression(rbp = 0)
-    t = @token
-    @token = next_token
-    left = t.nud(self)
-    more_expression(rbp, left)
-  end
-
-  def more_expression(rbp, left)
-    if rbp < @token.lbp
-      t = @token
-      @token = next_token
-      more_expression(rbp, t.led(self, left))
-    else
-      left
-    end
   end
 end
